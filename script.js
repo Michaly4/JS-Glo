ec29692e28cc5fc647afd8332ce1665c20851f8e
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-01 14:49:12 +0100

    prep

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-'use strick';
+'use strict';
 
 let num = [1,2,3,4,5];
 let res = 0;
 
 for (let index = 0; index < num.length; index++) {
-   res = res + num[index];
+   res += num[index];
 }
 console.log(res);
+console.log(res ** 3);
+console.log(res.substring(2));
