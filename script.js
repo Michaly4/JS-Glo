aa403c1f6caa5e03bee20933d804fb1feb8c23da
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-02 22:39:14 +0100

    korrektur

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 'use strict';
 
-let num = [1,2,3,4,5];
+let num = 266219;
+let numArray = num.toString().split("");
+console.log(numArray); 
+
 let res = 0;
 
-for (let index = 0; index < num.length; index++) {
-   res += num[index];
+for (let i = 0; i < numArray.length; i++) {
+   res += numArray[i];
 }
 console.log(res);
 console.log(String((res ** 3)).slice(0,2));
+
+
+
+
 /* let pow = res ** 3;
 console.log(String(pow).substring(0,2)); */
