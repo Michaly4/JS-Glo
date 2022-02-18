659991a0c90ed018e2f4880293474ee4e0c302b0
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-18 15:14:55 +0100

    lesson10

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 'use strict';
 
-let num = 266219;
-
-let numArray = num.toString().split('');
-console.log(numArray); 
-let res = [];
-
-for (let i = 0; i < numArray.length; i++) {
-   res *= numArray[i];
-}
-console.log(res);
-console.log(String((res ** 3)).slice(0,2));
 
 
-
-
-/* let pow = res ** 3;
-console.log(String(pow).substring(0,2)); */
