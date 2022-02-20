2390095aa1eacbe95e4c6b328211f8a91aa9e05e
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-20 09:33:25 +0100

    lesson10 prep

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const adv = document.querySelector(".adv");
 adv.remove();
 const body = document.body;
 body.style.backgroundImage = "url(./image/adv.jpg)";
-
 const titleTree = document.querySelector("h1");
 const books = document.querySelectorAll(".book");
 books[0].before(books[1]);
@@ -19,13 +18,19 @@ let book_2 = books[0].querySelectorAll("li");
 book_2[3].after(book_2[8]);
 book_2[5].after(book_2[6]);
 book_2[3].after(book_2[6]);
+book_2[10].before(book_2[2]);
 
 let book_5 = books[5].querySelectorAll("li");
 book_5[10].after(book_5[5]);
 book_5[1].after(book_5[9]);
 book_5[6].before(book_5[2]);
+book_5[6].before(book_5[2]);
+book_5[8].before(book_5[5]);
 
 let book_6 = books[2].querySelectorAll("li");
 let li = document.createElement("li", "Глава 8: За пределами ES6");
 li.textContent = "Глава 8: За пределами ES6";
 book_6[8].append(li);
+
+
+
