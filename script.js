7156e41caecce6a1e3de9ea8fba06a34476e45f5
Michaly4 <74726142+Michaly4@users.noreply.github.com>
2022-02-21 16:02:21 +0100

    News Prep Lesson11

--- script.js
+++ script.js
@@ -10,13 +10,47 @@ const kvd = document.querySelector("#square");
 const inpText = document.querySelector("#text")
 const pi = document.getElementById("circle");
 
+
+const btns = document.querySelectorAll('button')
+btns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    if(e.target.id == "btn"){
+      let valueInp = inpText.value;
+  kvd.style.backgroundColor = `${valueInp}`;
+  inpText.value = "";
+    }
+    else if(e.target.id == "e_btn"){
+      e_btn.style.display = "none";
+    }
+  })
+});
+range.addEventListener('input', change);
+function change() {
+  let rangeValue = range.value;
+  pi.style.width = `${rangeValue}%`;
+  pi.style.height = `${rangeValue}%`
+  rangeSpan.textContent = rangeValue + "%";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //  *** function ***
 function click() {
    let valueInp = inpText.value;
    kvd.style.backgroundColor = `${valueInp}`;
    inpText.value = "";
    e_btn.style.display = "none";
-   // pi.style.display = "none";
+   pi.style.display = "none";
 }
 
 
@@ -30,3 +64,4 @@ function change() {
 //  *** events ***
 btn.addEventListener('click', click);
 range.addEventListener('change', change);
+*/
